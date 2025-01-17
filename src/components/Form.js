c5936f8f39fad8622309de5964360fecbe284117
aubrey whitford <aubreya.whitford@gmail.com>
2025-01-17 11:27:09 -0500

    Completes lab

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [searchText, setSearchText] = useState("");
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,10 +13,18 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSearchChange(event) {
+    const newValue = event.target.value;
+    setSearchText(newValue);
+    props.onSearchChange(newValue);
+  }
+
+
   return (
     <form>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="text" onChange={handleSearchChange} value={searchText} />
       <button type="submit">Submit</button>
     </form>
   );
